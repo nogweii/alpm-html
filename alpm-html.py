5eee271ee31a9e177610682b75d6cc5111d839f7
Evaryont <colin@evaryont.me>
2020-12-03 21:26:58 -0700

    this is licensed under GPLv3

--- alpm-html.py
+++ alpm-html.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+
 # Generate a repository overview with per-package pages.
 
 import datetime
